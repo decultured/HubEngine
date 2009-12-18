99232e90dc88ef37571cf97ac0e9e6c3deadfb49
phil plante <mail@pplante.com>
2009-12-18 00:28:27 -0600

    added manifest.json and new deploy.sh script

--- Gaming/Aeroblox/deploy.sh
+++ Gaming/Aeroblox/deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # compile a "release" build
-# ./build_release
+./build_release
 
-# transfer the game assets to the server games storage
-# scp -r bin/* epaths01.endlesspaths.com:/var/www/onemorepoint.com/web/data/games/aeroblox/
 cd bin/
 zip -r ../package.zip ./*
 cd ../
-mv package.zip /tmp/
-# scp package.zip epaths01.endlesspaths.com:/tmp/
+
+# transfer the game assets to the server games storage
+scp package.zip onemorepoint.com:/tmp/
+
+# tell the server to pick up the package and deploy it.
 echo "Telling server to deploy:"
-curl -d package=package.zip http://pplante:als85buk@onemorepoint.local/api/game/aeroblox/deploy/
+curl -d package=package.zip http://apigamedeployer:jaSPuphUv6KuVefa8am7FEchaD9g8s6g@onemorepoint.com/api/game/aeroblox/deploy/
 echo ""
